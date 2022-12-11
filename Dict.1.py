226d1787ac370c1869701716aca96d205242d3ef
dj02468 <100090527+dj02468@users.noreply.github.com>
2022-12-11 18:43:53 +0100

    Add files via upload

---
+++ Dict.1.py
@@ -0,0 +1,10 @@
+deutschenglisch = {}    # Wörterbuch erstellen
+
+#Wösterbuch mit Inhalt füllen
+deutschenglisch['null'] = 'zero'
+deutschenglisch['eins'] = 'one'
+deutschenglisch['zwei'] = 'two'
+deutschenglisch['drei'] = 'three'
+
+#Ausdrucken
+print (deutschenglisch)
---
+++ Dict.2.py
@@ -0,0 +1,6 @@
+deutschitalienisch = {'eins': 'uno', 'zwei': 'due', 'drei': 'tre'}  #Dictionary anlegen und ausfüllen
+
+print(deutschitalienisch['zwei'] )
+
+
+
