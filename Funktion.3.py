5b4c445fce872982b43bbc7bb8f959730466093d
dj02468 <100090527+dj02468@users.noreply.github.com>
2023-01-09 11:46:47 +0100

    Add files via upload

---
+++ Funktion.3.py
@@ -0,0 +1,15 @@
+def mit_zwei_multiplizieren(eingabe):
+    eingabe = 2*eingabe
+    print("Verdopple ich diese Zahl, so erhalte ich ",eingabe)
+
+
+zahl = int(input("Gib eine ganze Zahl ein: "))
+mit_zwei_multiplizieren(zahl)
+zahl = int(input("Gib eine weitere ganze Zahl ein: "))
+mit_zwei_multiplizieren(zahl)
+
+print("Danke für die Eingabe.")
+
+#Schreibe eine Funktion summe(), welche für die Eingabe einer Zahl n folgendes Resultat ausgibt:
+
+#summe:=1+2+⋯+n
